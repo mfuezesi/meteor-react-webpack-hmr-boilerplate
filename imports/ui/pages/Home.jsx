8f9d7b7fae6f0c8e61221bb70e21b617b03611f3
Melchior Wom Füzesi <mfuezesi@gmail.com>
2016-08-19 00:30:00 +0200

    Fix for HMR

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import classNames from 'classnames';
 
-function Home() {
-  return (
-    <div className={classNames('Home', 'foo', 'bar')} >
-      <h1>Welcome!</h1>
-    </div>
-  );
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className={classNames('Home', 'foo', 'bar')} >
+        <h1>Welcome!</h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
