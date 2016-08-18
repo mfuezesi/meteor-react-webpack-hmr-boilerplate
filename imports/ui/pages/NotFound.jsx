58e4ac513fe8550d68346c6dedec59ef33efa111
Melchior Wom Füzesi <mfuezesi@gmail.com>
2016-08-19 00:30:28 +0200

    Fix for HMR

--- imports/ui/pages/NotFound.jsx
+++ imports/ui/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-function NotFound() {
-  return (
-    <div className='NotFound'>
-      <h1>This page can not be found</h1>
-    </div>
-  );
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div className='NotFound'>
+        <h1>This page can not be found :-(</h1>
+      </div>
+    );
+  }
 }
 
 export default NotFound;
